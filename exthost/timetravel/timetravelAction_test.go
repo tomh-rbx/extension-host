be0cf7b09dec615b11715a58d409f17563f03ebb
Ansgar Schulte <1299623+ansgarschulte@users.noreply.github.com>
2023-04-25 11:41:14 +0200

    test: timetravel unit test

---
+++ exthost/timetravel/timetravelAction_test.go
@@ -0,0 +1,77 @@
+package timetravel
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"github.com/steadybit/action-kit/go/action_kit_api/v2"
+	extension_kit "github.com/steadybit/extension-kit"
+	"github.com/steadybit/extension-kit/extutil"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestActionTimeTravel_Prepare(t *testing.T) {
+
+	tests := []struct {
+		name        string
+		requestBody action_kit_api.PrepareActionRequestBody
+		wantedError error
+		wantedState *ActionState
+	}{
+		{
+			name: "Should return config",
+			requestBody: action_kit_api.PrepareActionRequestBody{
+				Config: map[string]interface{}{
+					"action":     "prepare",
+					"duration":   "1000",
+					"offset":     "1000",
+					"disableNtp": "true",
+				},
+				ExecutionId: uuid.New(),
+			},
+
+			wantedState: &ActionState{
+				Offset:        1 * time.Second,
+				DisableNtp:    true,
+				OffsetApplied: false,
+			},
+		}, {
+			name: "Should return error too low duration",
+			requestBody: action_kit_api.PrepareActionRequestBody{
+				Config: map[string]interface{}{
+					"action":     "prepare",
+					"duration":   "0",
+					"offset":     "1000",
+					"disableNtp": "true",
+				},
+				ExecutionId: uuid.New(),
+			},
+
+			wantedError: extutil.Ptr(extension_kit.ToError("Duration must be greater / equal than 1s", nil)),
+		},
+	}
+	action := NewTimetravelAction()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			//Given
+			state := ActionState{}
+			request := tt.requestBody
+			//When
+			result, err := action.Prepare(context.Background(), &state, request)
+
+			//Then
+			if tt.wantedError != nil && err != nil {
+				assert.EqualError(t, err, tt.wantedError.Error())
+			} else if tt.wantedError != nil && result != nil {
+				assert.Equal(t, result.Error.Title, tt.wantedError.Error())
+			}
+			if tt.wantedState != nil {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.wantedState.OffsetApplied, state.OffsetApplied)
+				assert.Equal(t, tt.wantedState.Offset, state.Offset)
+				assert.Equal(t, tt.wantedState.DisableNtp, state.DisableNtp)
+			}
+		})
+	}
+}
