de4b9100f0366da163bfc386aca97ac448eb240a
Ansgar Schulte <1299623+ansgarschulte@users.noreply.github.com>
2023-04-27 15:43:52 +0200

    test: add process unit test

---
+++ exthost/process/process_util_test.go
@@ -0,0 +1,20 @@
+package stopprocess
+
+import (
+  "github.com/stretchr/testify/assert"
+  "os/exec"
+  "testing"
+)
+
+func TestStopProcesses(t *testing.T) {
+  command := exec.Command("tail", "-f", "/dev/null", "&")
+  err := command.Start()
+  assert.NoError(t, err)
+  ids := FindProcessIds("tail")
+  assert.Equal(t, 1, len(ids))
+  assert.Equal(t, command.Process.Pid, ids[0])
+  err = StopProcesses(ids, true)
+  assert.NoError(t, err)
+  ids = FindProcessIds("tail")
+  assert.Equal(t, 0, len(ids))
+}
