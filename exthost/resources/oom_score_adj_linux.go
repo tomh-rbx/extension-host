532ac0be2b2cee1de51824acea5982af0bf130ca
Ansgar Schulte <1299623+ansgarschulte@users.noreply.github.com>
2023-05-11 18:20:56 +0200

    chore: less log

--- exthost/resources/oom_score_adj_linux.go
+++ exthost/resources/oom_score_adj_linux.go
@@ -12,17 +12,16 @@ import (
 )
 
 func AdjustOOMScoreAdj() {
-  log.Info().Msg("Adjusting OOM score adj")
+  log.Debug().Msg("Adjusting OOM score adj")
   myPid := os.Getpid()
   path := "/proc/" + fmt.Sprintf("%d", myPid) + "/oom_score_adj"
-  out, err := common.RunAsRootWithResult("sh", "-c", "echo -997 > " + path)
+  err := common.RunAsRoot("sh", "-c", "echo -997 > " + path)
   if err != nil {
     log.Error().Err(err).Msg("Failed to adjust OOM score adj")
   }
-  log.Info().Msgf("OOM score adj result: %s", out)
   output, err := exec.Command("cat", path).CombinedOutput()
   if err != nil {
     return
   }
-  log.Info().Msgf("OOM score adj: %s", output)
+  log.Debug().Msgf("OOM score adj: %s", output)
 }
