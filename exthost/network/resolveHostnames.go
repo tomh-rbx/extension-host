d6a386bf730aac578bc6872092383ade7dd42a86
Johannes Edmeier <johannes.edmeier@steadybit.com>
2023-06-12 15:55:26 +0200

    fix: don't try to resolve empty hostnames

--- exthost/network/resolveHostnames.go
+++ exthost/network/resolveHostnames.go
@@ -22,6 +22,9 @@ func ResolveHostnames(ctx context.Context, ipOrHostnames ...string) ([]string, e
 
 	var sb strings.Builder
 	for _, hostname := range hostnames {
+		if len(hostname) == 0 {
+			continue
+		}
 		sb.WriteString(hostname)
 		sb.WriteString(" A\n")
 		sb.WriteString(hostname)
