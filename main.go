945b300013bef766c6a8041576b382d9c55b7704
Johannes Edmeier <johannes.edmeier@steadybit.com>
2023-10-07 11:38:12 +0200

    fix: parse configuration before starting probe http server to fail fast.

--- main.go
+++ main.go
@@ -37,15 +37,15 @@ func main() {
 	extbuild.PrintBuildInformation()
 	extruntime.LogRuntimeInformation(zerolog.InfoLevel)
 
-	//This will start /health/liveness and /health/readiness endpoints on port 8081 for use with kubernetes
-	//The port can be configured using the STEADYBIT_EXTENSION_HEALTH_PORT environment variable
-	exthealth.SetReady(false)
-	exthealth.StartProbes(int(config.Config.HealthPort))
-
 	// Most extensions require some form of configuration. These calls exist to parse and validate the
 	// configuration obtained from environment variables.
 	config.ParseConfiguration()
 	config.ValidateConfiguration()
+
+	//This will start /health/liveness and /health/readiness endpoints on port 8081 for use with kubernetes
+	//The port can be configured using the STEADYBIT_EXTENSION_HEALTH_PORT environment variable
+	exthealth.SetReady(false)
+	exthealth.StartProbes(int(config.Config.HealthPort))
 
 	// This call registers a handler for the extension's root path. This is the path initially accessed
 	// by the Steadybit agent to obtain the extension's capabilities.
