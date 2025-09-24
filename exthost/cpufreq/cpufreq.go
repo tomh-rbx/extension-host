392328467a9b52abd5257603e6bbc6675e038f6a
Tom Handal <thandal@roblox.com>
2025-09-24 09:22:44 -0700

    Changed Copyright and License Text

--- exthost/cpufreq/cpufreq.go
+++ exthost/cpufreq/cpufreq.go
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Copyright 2025 Roblox Corporation. All rights reserved.
+// SPDX-FileCopyrightText: Copyright 2025 Roblox Corporation. All rights reserved.
 // Author: Tom Handal <thandal@roblox.com>
 
 package cpufreq
